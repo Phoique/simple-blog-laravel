ad11d8dd33e25134da98354028e1d0114008514e
Enes Kahraman <official.phoique@gmail.com>
2019-07-23 01:05:57 +0300

    home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.layout')
+
+@section('title', 'Anasayfa')
+
+@section('content')
+
+@extends('partials.header')
+
+    <div class="row">
+        <div class="col-md-12 col-lg-6">
+            <div class="post-preview">
+                <a href="#">
+                    <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
+                    <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
+                </a>
+                <p class="post-meta">Posted by&nbsp;<a href="#">Start Bootstrap on September 24, 2018</a></p>
+            </div>
+            <hr>
+        </div>
+        <div class="col-md-12 col-lg-6">
+            <div class="post-preview">
+                <a href="#">
+                    <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
+                    <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
+                </a>
+                <p class="post-meta">Posted by&nbsp;<a href="#">Start Bootstrap on September 24, 2018</a></p>
+            </div>
+            <hr>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand" href="index.html">Laravel Blog</a>
+        <button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler"
+            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" href="index.html">anasayfa</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" href="about.html">hakkımızda</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" href="contact.html">iletişim</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" href="post.html">rasgele post</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" href="register.html">kayıt ol</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" href="login.html">Giriş yap</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
