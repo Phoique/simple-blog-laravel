0d1aadcab4f6174b88039aad8f89a64643046c21
Enes Kahraman <official.phoique@gmail.com>
2019-07-23 23:50:16 +0300

    register complated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,23 @@
-@extends('layouts.layout')
-@section('title', 'Anasayfa')
+@extends('layouts.app')
+
 @section('content')
-@section('headerTitle', 'Laravel Blog')
-@section('headerSub', 'Laravel ile kodlanmıştır.')
-<div class="row">
-    <div class="col-md-12 col-lg-6">
-        <div class="post-preview">
-            <a href="#">
-                <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
-                <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
-            </a>
-            <p class="post-meta">Posted by&nbsp;<a href="#">Start Bootstrap on September 24, 2018</a></p>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
         </div>
-        <hr>
-    </div>
-    <div class="col-md-12 col-lg-6">
-        <div class="post-preview">
-            <a href="#">
-                <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
-                <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
-            </a>
-            <p class="post-meta">Posted by&nbsp;<a href="#">Start Bootstrap on September 24, 2018</a></p>
-        </div>
-        <hr>
     </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,7 @@ Route::get('/', 'HomeController@index')->name('home');
 // Post detayına girildiğinde gösterilen route.
 Route::get('/post/{slug?}', 'PostController@postDetail')->name('detail');
 
-// Giriş route.
-Route::get('/login', 'LoginController@loginView')->name('login');
-
-// Kayıt route.
-Route::get('/register', 'RegisterController@index')->name('register');
-Route::post('/register', 'RegisterController@create')->name('register.create');
-
 // İçereği değişmeyen ve değer yollanmayan sabit sayfalar.
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
+Auth::routes();
